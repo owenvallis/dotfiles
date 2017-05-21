e8c32600aecdf835f6e7afebe0a9a29c71b827cb
owen vallis <owen@kadenze.com>
2017-05-20 21:02:06 -0700

    added after/ftplugin support and create settings for python and yaml

--- vim_runtime/install_vimrc.sh
+++ vim_runtime/install_vimrc.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 set -e
 
-cd ~/dotfiles/vim_runtime
-
 echo 'set runtimepath+=~/dotfiles/vim_runtime
+set runtimepath+=~/dotfiles/vim_runtime/after
+
 set packpath+=~/dotfiles/vim_runtime
 
 source ~/dotfiles/vim_runtime/vimrcs/vimrc.vim
-source ~/dotfiles/vim_runtime/vimrcs/filetypes.vim
 source ~/dotfiles/vim_runtime/vimrcs/plugins_config.vim' > ~/.vimrc
 
 echo "Installed the Vim configuration successfully!"
